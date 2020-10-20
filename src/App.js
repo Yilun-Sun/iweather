8708c124700f507e0c9c5f3b8616ef528b86faa3
YilunSun <osirisnero@gmail.com>
2020-10-19 22:35:44 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+import "antd/dist/antd.css";
+import { Input } from "antd";
+const { Search } = Input;
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      montrealWeather: {},
+      cityName: "",
+    };
+  }
+
+  componentDidMount() {
+    this.getCityLocationByName("montreal");
+  }
+
+  getCityLocationByName = (cityName) => {
+    this.setState({ cityName: cityName });
+    const cityLookupUrl = "https://geoapi.qweather.com/v2/city/lookup?";
+
+    // get location
+    axios
+      .get(cityLookupUrl, {
+        params: {
+          key: "935d116f4c384dec9ac8e88bdba1319d",
+          location: cityName,
+          lang: "en",
+        },
+      })
+      .then((response) => {
+        // this.setState({ montrealWeather: response.data.now });
+        console.log(response.data.location[0]);
+        this.getWeatherDataByLocation(
+          response.data.location[0].lon + "," + response.data.location[0].lat
+        );
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  getWeatherDataByLocation = (location) => {
+    const weatherUrl = "https://devapi.qweather.com/v7/weather/now?";
+
+    // get weather by location
+    axios
+      .get(weatherUrl, {
+        params: {
+          key: "935d116f4c384dec9ac8e88bdba1319d",
+          location: location,
+          lang: "en",
+        },
+      })
+      .then((response) => {
+        this.setState({ montrealWeather: response.data.now });
+        console.log(response.data.now);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  onSearch = (value) => {
+    this.getCityLocationByName(value);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Météo</h1>
+        <Search
+          placeholder="input city name"
+          onSearch={this.onSearch}
+          style={{ width: 200 }}
+        />
+        <div>City: {this.state.cityName}</div>
+        <div>Temperature: {this.state.montrealWeather.temp}℃</div>
+        <div>Feels like: {this.state.montrealWeather.feelsLike}℃</div>
+        <div>Outside: {this.state.montrealWeather.text}</div>
+        <div>Pressure: {this.state.montrealWeather.pressure}</div>
+        <div>Visibility: {this.state.montrealWeather.vis}</div>
+        <div>Observe time: {this.state.montrealWeather.obsTime}</div>
+      </div>
+    );
+  }
 }
-
-export default App;
